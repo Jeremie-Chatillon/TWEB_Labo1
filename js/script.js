4c5e5b4c603a0eb36a137034c172429293e29bf6
Unknown <jeremie.chatillon@heig-vd.ch>
2018-09-25 13:19:58 +0200

    Final
    no extra comment

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 let i = true;
 let timer = 15;
 const congratulations = '<br>You did it fine. Congratulations'
-const noCSS = 'Ohhhh noooo, what did you do???? CSS disaperad. You see? Wasn\'t it better before';
-const withCSS = 'This post is relating about how important to have a nice webPage. I can say a lot of bullshit, but the better way of realise it is to test it... So let try. </br><br>However, you have to find the buton to disable it ;D </br>I\'m sure you can do dat easily.';
+const noCSS = 'Ohhhh noooo, what did you do???? CSS has disappeared. You see? Isn\'t it better before??';
+const withCSS = 'This article discusses the importance of having a beautiful web page. Even if the creator of the site has dubious tastes ... I can still say a lot of bullshit, but the best way to realize is to test it ... So, let\'s try. <br/><br/>However, you must find the buton to disable the CSS before the end of the timer. <br/>I\'m sure you can do it easily.';
+
+window.onload = function  initialiser() {
+  document.getElementById('text').innerHTML = withCSS;
+};
+
+// disable and enable CSS
 //https://stackoverflow.com/questions/5278062/how-could-i-turn-off-a-stylesheethttps://stackoverflow.com/questions/5278062/how-could-i-turn-off-a-stylesheet
 function doMagic() {
   
   if(timer <= 0){
     console.log(timer); 
-    document.getElementsByTagName('body')[0].innerHTML = 'tooLate :S';
+    document.getElementsByTagName('body')[0].innerHTML = 'too Late :S';
   } else{
 
     clearInterval(x);
 
-    document.getElementById('timer').innerHTML = "<br>Too Late:S";  /// ça passe pas :()
+    document.getElementById('timer').innerHTML = congratulations; 
       
     if(i == true){
       for ( i=0; i<document.styleSheets.length; i++) 
@@ -27,15 +33,13 @@ function doMagic() {
         document.getElementById('text').innerHTML = withCSS;
     }
   } 
-}
+};
     
-
-
 // Timer: https://www.w3schools.com/howto/howto_js_countdown.asp
 var x = setInterval(function() {
  
   timer -= 1; 
-  document.getElementById('timer').innerHTML =  timer + "s ";
+  document.getElementById('timer').innerHTML =  timer + "s "; 
   
   console.log(timer);
   // If the count down is over, write some text 
